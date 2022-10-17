c752aacc91934560cbf9425fda622640bd0abc1c
Bunyameen <bunyameen@gmail.com>
2022-10-17 09:43:36 +0100

    UseEffect with DataFetching

---
+++ use-effect/src/components/DataFetching.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+
+const DataFetching = () => {
+  const [posts, setPosts] = useState([])
+
+  const apiURL = "https://jsonplaceholder.typicode.com/posts"
+
+
+  useEffect(() => {
+    fetch(apiURL)
+    .then((response) => response.json())
+    .then((result) =>{
+      setPosts(result)
+      console.log(result)
+    })
+  }, [])
+
+  return (
+    <div>
+      <ul>
+        {
+          posts.map((post) => <li key={post.id}>{post.title}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default DataFetching
