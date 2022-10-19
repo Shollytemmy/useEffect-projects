0a40f6a6e7345e320fe288236abf3679fc9eac7f
Bunyameen <bunyameen@gmail.com>
2022-10-19 11:44:39 +0100

    contionally rendering effect hook

--- use-effect/src/components/UpdateTitle.js
+++ use-effect/src/components/UpdateTitle.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 
 const UpdateTitle = () => {
     const [count, setCount] = useState(0)
-
+    const [name, setName] = useState('')
     
-    useEffect(() => {
-        document.title = `you click ${count} times`
 
-    })
+   
+    useEffect(() => {
+        console.log("UseEffect is updating")
+         document.title = `you click ${count} times`
+
+    }, [count])
   return (
     <div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <button onClick={() => setCount(count + 1)}>Click {count}</button>
     </div>
   )
